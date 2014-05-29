53ac02cbb33a0b6d9082b9265c792d8403d67dea
Miroslaw Karpiewski <mirek0922@gmail.com>
2014-05-29 17:46:06 +0200

    Added preferences

---
+++ src/pwr/project/getrawdata/Preferences.java
@@ -0,0 +1,14 @@
+package pwr.project.getrawdata;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefs);
+	}
+}

b5f5fbfe9ae2fa1bc8eacc3c9b4722473bd2ea93
Miroslaw Karpiewski <mirek0922@gmail.com>
2014-06-01 13:00:50 +0200

    SERVICE work done

--- src/pwr/project/getrawdata/GetRawData.java
+++ src/pwr/project/getrawdata/GetRawData.java
@@ -3,12 +3,17 @@ package pwr.project.getrawdata;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -33,34 +38,29 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class GetRawData extends Activity implements OnClickListener,
-		SensorEventListener, LocationListener {
+public class GetRawData extends Activity implements OnClickListener {
 
 	Button butShowLog;
 	Button butClearLog;
-	private Sensor accelerometer;
-	private Srednia<WynikiACC> sr;
 	private SharedPreferences prefs;
-	private SensorManager mSensorManager;
-	private LocationManager mLocationManager;
-	private Location middleOfSafeZone;
-	private float maximumSafeDistance = 1000;
-	private boolean isMonitoring = false;
+	static float maximumSafeDistance = 1000;
 	static final float threshold = 3.5f;
 	private ToggleButton butStart;
 	ImageView view;
+	static boolean isRunning = false;
 	final static String messageTextPreference = "textMessage";
 	final static String sendPreference = "sendMessage";
 	final static String telephonNumberPreference = "telephoneNumber";
 	final static String maxDistPreference = "safeDistance";
-	private static boolean isSendingMessage;
-	private ArrayList<String> LogList;
-	private String messageText;
-	private Location currentLocation;
+	static boolean isSendingMessage;
+	private static ArrayList<String> LogList;
+	private AlarmReciver alarms;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		
 		setContentView(R.layout.main);
 		butStart = (ToggleButton) findViewById(R.id.butStart);
 		butShowLog = (Button) findViewById(R.id.butLog);
@@ -71,209 +71,100 @@ public class GetRawData extends Activity implements OnClickListener,
 		butClearLog.setOnClickListener(this);
 		butShowLog.setOnClickListener(this);
 		view = (ImageView) findViewById(R.id.imageView1);
-		sr = new Srednia<WynikiACC>();
-		mSensorManager = (SensorManager) this
-				.getSystemService(Context.SENSOR_SERVICE);
-		accelerometer = mSensorManager
-				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		if (accelerometer == null) {
-			Toast.makeText(this, "No accelerometer available",
-					Toast.LENGTH_SHORT).show();
-		}
-		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		if (mLocationManager == null) {
-			Toast.makeText(this, "No GPS available", Toast.LENGTH_SHORT).show();
-			finish();
-		}
+		isRunning = true;
+		Intent i = getIntent();
+		boolean isAlarm = i.getBooleanExtra(MonitoringService.ALARM, false);
+		adjustUserInterface(isAlarm);
 	}
+	
+
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		mSensorManager.unregisterListener(this);
-		mLocationManager.removeUpdates(this);
+		if (alarms != null)
+			unregisterReceiver(alarms);
+	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		stopService(new Intent(this, MonitoringService.class));
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		isRunning = false;
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		mSensorManager.registerListener(this, accelerometer,
-				SensorManager.SENSOR_DELAY_FASTEST);
-		isSendingMessage = prefs.getBoolean(sendPreference, false);
-		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-				0, 0, this);
+		if (isMyServiceRunning()) {
+			view.setImageDrawable(getResources().getDrawable(R.drawable.ok));
+			if (!butStart.isChecked()) {
+				butStart.setChecked(true);
+			}
 
-		middleOfSafeZone = mLocationManager
-				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-		if (middleOfSafeZone != null) {
-			Log.i(getClass().getSimpleName(),
-					"middleOfSafeZone = " + middleOfSafeZone.getLongitude()
-							+ " " + middleOfSafeZone.getLatitude());
 		}
+		isSendingMessage = prefs.getBoolean(sendPreference, false);
 		String maxDist = prefs.getString(maxDistPreference, "1000");
 		maximumSafeDistance = Float.parseFloat(maxDist);
-		messageText = prefs.getString(messageTextPreference, "ratunku!");
 
+		if (alarms == null)
+			alarms = new AlarmReciver();
+		IntentFilter intentFilter = new IntentFilter(MonitoringService.ALARM);
+		registerReceiver(alarms, intentFilter);
 	}
 
 	@Override
 	public void onClick(View v) {
 		if (v == butStart) {
 			if (butStart.isChecked()) {
+				view.setImageDrawable(getResources().getDrawable(R.drawable.ok));
 				Intent i = new Intent(this, MonitoringService.class);
 				startService(i);
-//				isMonitoring = true;
-//				middleOfSafeZone = mLocationManager
-//						.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//				startAccelerometerAndAdjustUI();
-//				currentLocation = middleOfSafeZone;
-//			} else {
-//				isMonitoring = false;
-//				stopAccelerometerAndAdjustUI();
+			} else {
+				adjustUserInterface(false);
+				stopService(new Intent(this, MonitoringService.class));
 			}
-			
+
 		} else if (v == butShowLog) {
 			final Dialog dial = createAlertDialog();
 			dial.show();
-			
+
 		} else if (v == butClearLog) {
 			LogList.clear();
 			Toast.makeText(this, "Log cleared", Toast.LENGTH_SHORT).show();
 		}
 
 	}
-	
-	private Dialog createAlertDialog(){
+
+	private Dialog createAlertDialog() {
 		AlertDialog.Builder logDialog = new AlertDialog.Builder(this);
 		logDialog.setTitle("Log akcelerometru");
 		ListView logListView = new ListView(this);
-		ArrayAdapter<String> logListAdapter = new ArrayAdapter<String>(
-				this, android.R.layout.simple_list_item_1,
-				android.R.id.text1, LogList);
+		ArrayAdapter<String> logListAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1,
+				LogList);
 		logListView.setAdapter(logListAdapter);
 		logDialog.setView(logListView);
 		return logDialog.create();
 	}
 
-	void startAccelerometerAndAdjustUI() {
-		view.setImageDrawable(getResources().getDrawable(R.drawable.ok));
-		mSensorManager.registerListener(this, accelerometer,
-				SensorManager.SENSOR_DELAY_FASTEST);
-
-	}
-
-	void stopAccelerometerAndAdjustUI() {
-		view.setImageDrawable(getResources().getDrawable(R.drawable.klepsydra));
-		mSensorManager.unregisterListener(this);
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	}
-
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			float rawX = event.values[0];
-			float rawY = event.values[1];
-			float rawZ = event.values[2];
-
-			float srednia = sr.sredniaCalkowitaZWieluPomiarow(new WynikiACC(
-					rawX, rawY, rawZ));
-			log("Srednia = " + srednia);
-			if (srednia < threshold && isMonitoring) {
-				raiseAlarm();
-			}
-		}
-	}
-
-	private void raiseAlarm() {
-		view.setImageDrawable(getResources().getDrawable(R.drawable.error));
-		maintainAlarm();
-
-	}
-
-	private void maintainAlarm() {
-		sendMessage();
-		sr.wynikiPomiarow.clear();
-		isMonitoring = false;
-		mSensorManager.unregisterListener(this);
-
-	}
-
-	private void sendMessage() {
-		Log.i(getClass().getSimpleName(), parseTextMessage(messageText));
-
-		if (isSendingMessage) {
-			String telephonNumber = prefs.getString(telephonNumberPreference,
-					"000000000");
-			parseTextMessage(messageText);
-			SmsManager sms = SmsManager.getDefault();
-			sms.sendTextMessage(telephonNumber, null, messageText, null, null);
-			Log.i(getClass().getSimpleName(), "Message sent to: "
-					+ telephonNumber);
-			Toast.makeText(this, "ALARM, message sento to = " + telephonNumber,
-					Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	@SuppressLint("DefaultLocale")
-	private String parseTextMessage(String message) {
-		String resultMessage;
-		if (currentLocation != null) {
-			resultMessage = message.replace("%l", "" + "dlugosc = "
-					+ currentLocation.getLongitude() + " szerokosc = "
-					+ currentLocation.getLatitude());
-		} else {
-			resultMessage = message.replace("%l",
-					"\"Brak informacji o lokalizacji!\"");
-		}
-		Calendar c = Calendar.getInstance();
-		String date = String.format(Locale.ENGLISH, "%d:%d:%d %d/%d/%d",
-				c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE),
-				c.get(Calendar.SECOND), c.get(Calendar.DAY_OF_MONTH),
-				c.get(Calendar.MONTH), c.get(Calendar.YEAR));
-
-		resultMessage = resultMessage.replace("%d", date);
-		return resultMessage;
-	}
 
 	void adjustUserInterface(boolean isTriggerd) {
 		if (isTriggerd) {
 			view.setImageDrawable(getResources().getDrawable(R.drawable.error));
+			if(!butStart.isChecked()){
+				butStart.setChecked(true);
+			}
+		} else {
+			view.setImageDrawable(getResources().getDrawable(
+					R.drawable.klepsydra));
+
 		}
-	}
-
-	@Override
-	public void onLocationChanged(Location location) {
-		Log.i(getClass().getSimpleName(),
-				"location = " + location.getLongitude() + " "
-						+ location.getLatitude());
-		currentLocation = location;
-
-		inSafeZone(location);
-	}
-
-	private void inSafeZone(Location actual) {
-		if (actual.distanceTo(middleOfSafeZone) > maximumSafeDistance
-				&& isMonitoring) {
-			raiseAlarm();
-		}
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
 	}
 
 	@Override
@@ -295,7 +186,28 @@ public class GetRawData extends Activity implements OnClickListener,
 
 	}
 
-	private void log(String s) {
+	static void log(String s) {
 		LogList.add(s);
+	}
+
+	private class AlarmReciver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (intent.getAction().equals(MonitoringService.ALARM)) {
+				adjustUserInterface(true);
+			}
+		}
+	}
+
+	private boolean isMyServiceRunning() {
+		ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		for (RunningServiceInfo service : manager
+				.getRunningServices(Integer.MAX_VALUE)) {
+			if (MonitoringService.class.getName().equals(
+					service.service.getClassName())) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- src/pwr/project/getrawdata/MonitoringService.java
+++ src/pwr/project/getrawdata/MonitoringService.java
@@ -1,35 +1,56 @@
 package pwr.project.getrawdata;
 
+import java.util.Calendar;
+import java.util.Locale;
+
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Toast;
 
-public class MonitoringService extends Service implements SensorEventListener {
-	
+public class MonitoringService extends Service implements SensorEventListener,
+		LocationListener {
+
 	Srednia<WynikiACC> sredniaZPomiarow;
 	private SensorManager mSensorManager;
+	private LocationManager mLocationManager;
 	private Sensor accelerometer;
+	private Location currentLocation = null;
+	SharedPreferences prefs;
+	private Location middleOfSafeZone;
+	static final String ALARM = "ALARM";
 
-	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		sredniaZPomiarow = new Srednia<WynikiACC>();
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				0, 0, this);
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		mSensorManager = (SensorManager) this
 				.getSystemService(Context.SENSOR_SERVICE);
 		accelerometer = mSensorManager
 				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		mSensorManager.registerListener(this, accelerometer,
-				SensorManager.SENSOR_DELAY_FASTEST);	
+				SensorManager.SENSOR_DELAY_FASTEST);
+		middleOfSafeZone = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 		return super.onStartCommand(intent, flags, startId);
-	
+
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -42,25 +63,121 @@ public class MonitoringService extends Service implements SensorEventListener {
 			float rawY = event.values[1];
 			float rawZ = event.values[2];
 
-			float srednia = sredniaZPomiarow.sredniaCalkowitaZWieluPomiarow(new WynikiACC(
-					rawX, rawY, rawZ));
+			float srednia = sredniaZPomiarow
+					.sredniaCalkowitaZWieluPomiarow(new WynikiACC(rawX, rawY,
+							rawZ));
 
-			//			log("Srednia = " + srednia);
-			if (srednia < GetRawData.threshold ) {
+			GetRawData.log("Srednia = " + srednia);
+			if (srednia < GetRawData.threshold) {
 				raiseAlarm();
 			}
-		}		
+		}
 	}
 
-	private void raiseAlarm(){
-		Log.i(getClass().getSimpleName(), "YUPII");
-		mSensorManager.unregisterListener(this);
+	private void raiseAlarm() {
+		if(GetRawData.isRunning)
+			sendBroadcast(new Intent(ALARM));
+		else{
+			Intent i = new Intent(MonitoringService.this, GetRawData.class);
+			i.putExtra(ALARM, true);
+			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(i);
+		}
+		maintainAlarm();
 		stopSelf();
 	}
-	
+
+	private void maintainAlarm() {
+		sendMessage();
+		sredniaZPomiarow.wynikiPomiarow.clear();
+		mSensorManager.unregisterListener(this);
+		mLocationManager.removeUpdates(this);
+
+	}
+
+	@Override
+	public void onDestroy() {
+		mSensorManager.unregisterListener(this);
+		mLocationManager.removeUpdates(this);
+		super.onDestroy();
+
+	}
+
+	private void sendMessage() {
+		String messageText = prefs.getString(GetRawData.messageTextPreference,
+				"");
+		Log.i(getClass().getSimpleName(), parseTextMessage(messageText));
+
+		if (GetRawData.isSendingMessage) {
+			String telephonNumber = prefs.getString(
+					GetRawData.telephonNumberPreference, "000000000");
+			messageText = parseTextMessage(messageText);
+			// SmsManager sms = SmsManager.getDefault();
+			// sms.sendTextMessage(telephonNumber, null, messageText, null,
+			// null);
+			Log.i(getClass().getSimpleName(), "Message sent to: "
+					+ telephonNumber);
+			Log.i(getClass().getSimpleName(), "Message sent: " + messageText);
+			Toast.makeText(this, "ALARM, message sento to = " + telephonNumber,
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	@SuppressLint("DefaultLocale")
+	private String parseTextMessage(String message) {
+		String resultMessage;
+		if (currentLocation != null) {
+			resultMessage = message.replace("%l", "" + "dlugosc = "
+					+ currentLocation.getLongitude() + " szerokosc = "
+					+ currentLocation.getLatitude());
+		} else {
+			resultMessage = message.replace("%l",
+					"\"Brak informacji o lokalizacji!\"");
+		}
+		Calendar c = Calendar.getInstance();
+		String date = String.format(Locale.getDefault(), "%d:%d:%d %d/%d/%d",
+				c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE),
+				c.get(Calendar.SECOND), c.get(Calendar.DAY_OF_MONTH),
+				c.get(Calendar.MONTH), c.get(Calendar.YEAR));
+
+		resultMessage = resultMessage.replace("%d", date);
+		return resultMessage;
+	}
+
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		currentLocation = location;
+
+		inSafeZone(location);
+	}
+
+	private void inSafeZone(Location actual) {
+		if (actual.distanceTo(middleOfSafeZone) > GetRawData.maximumSafeDistance) {
+			raiseAlarm();
+		}
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
